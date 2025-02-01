2a62ca34d92f9f81db43468aa8001d73a76a62b3
liliml <lilianml@uw.edu>
2025-02-01 13:31:47 -0800

    got sorting table to work

--- js/earthquake.js
+++ js/earthquake.js
@@ -11,7 +11,7 @@ let map = new mapboxgl.Map({
 // Below is step 5 in the lab
 async function geojsonFetch() { 
     // fetch geojson
-    let response, nationalforests, capitals, usstates, table, tablecapital;
+    let response, nationalforests, capitals, usstates, table;
     response = await fetch('assets/FS_National_Forests.geojson');
     nationalforests = await response.json();
     response = await fetch('assets/capitals.geojson');
@@ -45,14 +45,11 @@ async function geojsonFetch() {
     
         map.addLayer({
             'id': 'nationalforests-layer',
-            'type': 'circle',
+            'type': 'fill',
             'source': 'nationalforests',
             'paint': {
-                'circle-radius': 4,
-                'circle-stroke-width': 2,
-                'circle-color': 'green',
-                'circle-stroke-color': 'black',
-                'circle-opacity': 0.5
+                'fill-color': 'green', // green color fill
+                'fill-opacity': 1
             }
         });
     
@@ -72,6 +69,18 @@ async function geojsonFetch() {
                 'circle-stroke-color': 'white'
             }
         });
+
+        // map.addLayer({
+        //     'id': 'capitals-labels',
+        //     'type': 'symbol',
+        //     'source': 'capitals',
+        //     'paint': {
+        //         'symbol-placement':'point',
+        //         'text-anchor':'center',
+        //         'text-color':'white'
+        //         // 'text-fields'
+        //     }
+        // });
         
     
     });
@@ -85,44 +94,25 @@ async function geojsonFetch() {
         cell1 = row.insertCell(0);
         cell2 = row.insertCell(1);
         cell3 = row.insertCell(2);
-        cell1.innerHTML = nationalforests.features[i].properties.OBJECTID;
-        cell2.innerHTML = nationalforests.features[i].properties.FORESTNAME;
+        cell1.innerHTML = nationalforests.features[i].properties.FORESTNAME;
+        cell2.innerHTML = nationalforests.features[i].properties.OBJECTID;
         // cell3.innerHTML = new Date(nationalforests.features[i].properties.time).toLocaleDateString(
             // "en-US");
     }
     
     // step 8 in the lab
-    let btn = document.getElementsByTagName("button")[0];
+    let btn = document.getElementsByTagName("button")[0]; //gets first button with tag button from the html, the 0 is for indexing purposes for if there is multiple buttons
     
     btn.addEventListener('click', sortTable);
 
 
-    //second portion for testing, can remove this potentially for testing
-    // for capitals of states table in section below
-    tablecapital = document.getElementsByTagName("table")[0];
-    let row, cell1, cell2, cell3;
-    for (let i = 0; i < capitals.features.length; i++) {
-        // Create an empty <tr> element and add it to the 1st position of the table:
-        row = table.insertRow(-1);
-        cell1 = row.insertCell(0);
-        cell2 = row.insertCell(1);
-        cell3 = row.insertCell(2);
-        cell1.innerHTML = capitals.features[i].properties.state;
-        cell2.innerHTML = capitals.features[i].properties.city;
-        // cell3.innerHTML = new Date(nationalforests.features[i].properties.time).toLocaleDateString(
-            // "en-US");
-    }
-    
-    // step 8 in the lab
-    let btn = document.getElementsByTagName("button")[0];
-    
-    btn.addEventListener('click', sortTable);
 };
 
 // define the function to sort table
 function sortTable(e) {
     let table, rows, switching, i, x, y, shouldSwitch;
     table = document.getElementsByTagName("table")[0];
+    //FOR TESTING: console.log(table)
     switching = true;
     /*Make a loop that will continue until
     no switching has been done:*/
@@ -137,10 +127,21 @@ function sortTable(e) {
             shouldSwitch = false;
             /*Get the two elements you want to compare,
             one from current row and one from the next:*/
-            x = parseFloat(rows[i].getElementsByTagName("td")[1].innerHTML);
-            y = parseFloat(rows[i + 1].getElementsByTagName("td")[1].innerHTML);
+            //ORIGINAL: x = parseFloat(rows[i].getElementsByTagName("td")[0].innerHTML);
+            //ORIGINAL: y = parseFloat(rows[i + 1].getElementsByTagName("td")[0].innerHTML);
+            x = rows[i].getElementsByTagName("td")[0].innerHTML; //DO NOT NEED parseFloat here as it takes string and parses it into a float
+            y = rows[i + 1].getElementsByTagName("td")[0].innerHTML; //0 is the table data index, aka the column number here, and we want first column aka index 0
+
+            //FOR TESTING: console.log(x);
+            //FOR TESTING: console.log(y);
+
             //check if the two rows should switch place:
-            if (x < y) {
+            if (x.toLowerCase() > y.toLowerCase()) {
+            //in line above, removed .innerHTML as was not needed and caused errors as would not even be able to reach/parse 
+            //this if statement correctly, it thought toLowerCase was the issue, but .innerHTML was the issue. Not fully sure why it was unneeded though 
+            //or why it caused issues, I think it was as we already had x and y, didn't need to attempt to grab the html again?
+
+            // changed line above to this for alphabetical sorting, see this source: https://www.w3schools.com/howto/howto_js_sort_table.asp
                 //if so, mark as a switch and break the loop:
                 shouldSwitch = true;
                 break;
