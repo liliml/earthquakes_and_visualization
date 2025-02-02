f8c6d80a76611efea4678794048b3546034fb969
liliml <lilianml@uw.edu>
2025-02-01 16:07:38 -0800

    switched index and earthquakes files

--- js/earthquake.js
+++ js/earthquake.js
@@ -3,84 +3,59 @@ mapboxgl.accessToken = 'pk.eyJ1Ijoicm9zZS0xNjgiLCJhIjoiY202aWYxY3lsMDdxdjJpcHJoa
 
 let map = new mapboxgl.Map({
     container: 'map', // container ID
-    style: 'mapbox://styles/mapbox/navigation-night-v1', // style URL, is of navigation night
-    zoom: 4.5, // starting zoom
-    center: [265, 38] // starting center
+    style: 'mapbox://styles/mapbox/satellite-v9', // style URL
+    zoom: 5.5, // starting zoom
+    center: [138, 38] // starting center
 });
 
 // Below is step 5 in the lab
 async function geojsonFetch() { 
     // fetch geojson
-    let response, nationalforests, capitals, usstates, table;
-    response = await fetch('assets/FS_National_Forests.geojson');
-    nationalforests = await response.json();
-    response = await fetch('assets/capitals.geojson');
-    capitals = await response.json();
-    response = await fetch('assets/us-states.json');
-    usstates = await response.json();
+    let response, earthquakes, japan, table;
+    response = await fetch('assets/earthquakes.geojson');
+    earthquakes = await response.json();
+    response = await fetch('assets/earthquakes.geojson');
+    earthquakes = await response.json();
+    response = await fetch('assets/japan.json');
+    japan = await response.json();
 
 
     // Below is step 6 in the lab
     //load data to the map as new layers and table on the side.
     map.on('load', function loadingData() {
-        map.addSource('usstates', {
+        map.addSource('japan', {
             type: 'geojson',
-            data: usstates
+            data: japan
         });
     
         map.addLayer({
-            'id': 'usstates-layer',
+            'id': 'japan-layer',
             'type': 'fill',
-            'source': 'usstates',
+            'source': 'japan',
             'paint': {
-                'fill-color': '#A020F0', // purple color fill
+                'fill-color': '#0080ff', // blue color fill
                 'fill-opacity': 0.5
             }
         });
     
-        map.addSource('nationalforests', {
+        map.addSource('earthquakes', {
             type: 'geojson',
-            data: nationalforests
+            data: earthquakes
         });
     
         map.addLayer({
-            'id': 'nationalforests-layer',
-            'type': 'fill',
-            'source': 'nationalforests',
-            'paint': {
-                'fill-color': 'green', // green color fill
-                'fill-opacity': 1
-            }
-        });
-    
-        map.addSource('capitals', {
-            type: 'geojson',
-            data: capitals
-        });
-    
-        map.addLayer({
-            'id': 'capitals-layer',
+            'id': 'earthquakes-layer',
             'type': 'circle',
-            'source': 'capitals',
+            'source': 'earthquakes',
             'paint': {
                 'circle-radius': 8,
                 'circle-stroke-width': 2,
-                'circle-color': 'blue',
+                'circle-color': 'red',
                 'circle-stroke-color': 'white'
             }
         });
-
-        // map.addLayer({
-        //     'id': 'capitals-labels',
-        //     'type': 'symbol',
-        //     'source': 'capitals',
-        //     'paint': {
-        //         'symbol-placement':'point',
-        //         'text-anchor':'center',
-        //         'text-color':'white'
-        //         // 'text-fields'
-        //     }
-        // });
+    
+    
         
     
     });
@@ -88,32 +63,30 @@ async function geojsonFetch() {
     // step 7 in the lab
     table = document.getElementsByTagName("table")[0];
     let row, cell1, cell2, cell3;
-    for (let i = 0; i < nationalforests.features.length; i++) {
+    for (let i = 0; i < earthquakes.features.length; i++) {
         // Create an empty <tr> element and add it to the 1st position of the table:
         row = table.insertRow(-1);
         cell1 = row.insertCell(0);
         cell2 = row.insertCell(1);
         cell3 = row.insertCell(2);
-        cell1.innerHTML = nationalforests.features[i].properties.FORESTNAME;
-        cell2.innerHTML = nationalforests.features[i].properties.OBJECTID;
-        cell3.innerHTML = nationalforests.features[i].geometry.coordinates;
-        // cell3.innerHTML = new Date(nationalforests.features[i].properties.time).toLocaleDateString(
-            // "en-US");
+        cell1.innerHTML = earthquakes.features[i].properties.id;
+        cell2.innerHTML = earthquakes.features[i].properties.mag;
+        cell3.innerHTML = new Date(earthquakes.features[i].properties.time).toLocaleDateString(
+            "en-US");
     }
     
     // step 8 in the lab
-    let btn = document.getElementsByTagName("button")[0]; //gets first button with tag button from the html, the 0 is for indexing purposes for if there is multiple buttons
+    let btn = document.getElementsByTagName("button")[0];
     
     btn.addEventListener('click', sortTable);
 
-
+    
 };
 
 // define the function to sort table
 function sortTable(e) {
     let table, rows, switching, i, x, y, shouldSwitch;
     table = document.getElementsByTagName("table")[0];
-    //FOR TESTING: console.log(table)
     switching = true;
     /*Make a loop that will continue until
     no switching has been done:*/
@@ -128,21 +101,10 @@ function sortTable(e) {
             shouldSwitch = false;
             /*Get the two elements you want to compare,
             one from current row and one from the next:*/
-            //ORIGINAL: x = parseFloat(rows[i].getElementsByTagName("td")[0].innerHTML);
-            //ORIGINAL: y = parseFloat(rows[i + 1].getElementsByTagName("td")[0].innerHTML);
-            x = rows[i].getElementsByTagName("td")[0].innerHTML; //DO NOT NEED parseFloat here as it takes string and parses it into a float
-            y = rows[i + 1].getElementsByTagName("td")[0].innerHTML; //0 is the table data index, aka the column number here, and we want first column aka index 0
-
-            //FOR TESTING: console.log(x);
-            //FOR TESTING: console.log(y);
-
+            x = parseFloat(rows[i].getElementsByTagName("td")[1].innerHTML);
+            y = parseFloat(rows[i + 1].getElementsByTagName("td")[1].innerHTML);
             //check if the two rows should switch place:
-            if (x.toLowerCase() > y.toLowerCase()) {
-            //in line above, removed .innerHTML as was not needed and caused errors as would not even be able to reach/parse 
-            //this if statement correctly, it thought toLowerCase was the issue, but .innerHTML was the issue. Not fully sure why it was unneeded though 
-            //or why it caused issues, I think it was as we already had x and y, didn't need to attempt to grab the html again?
-
-            // changed line above to this for alphabetical sorting, see this source: https://www.w3schools.com/howto/howto_js_sort_table.asp
+            if (x < y) {
                 //if so, mark as a switch and break the loop:
                 shouldSwitch = true;
                 break;
--- js/index.js
+++ js/index.js
@@ -3,59 +3,84 @@ mapboxgl.accessToken = 'pk.eyJ1Ijoicm9zZS0xNjgiLCJhIjoiY202aWYxY3lsMDdxdjJpcHJoa
 
 let map = new mapboxgl.Map({
     container: 'map', // container ID
-    style: 'mapbox://styles/mapbox/satellite-v9', // style URL
-    zoom: 5.5, // starting zoom
-    center: [138, 38] // starting center
+    style: 'mapbox://styles/mapbox/navigation-night-v1', // style URL, is of navigation night
+    zoom: 4.5, // starting zoom
+    center: [265, 38] // starting center
 });
 
 // Below is step 5 in the lab
 async function geojsonFetch() { 
     // fetch geojson
-    let response, earthquakes, japan, table;
-    response = await fetch('assets/earthquakes.geojson');
-    earthquakes = await response.json();
-    response = await fetch('assets/earthquakes.geojson');
-    earthquakes = await response.json();
-    response = await fetch('assets/japan.json');
-    japan = await response.json();
+    let response, nationalforests, capitals, usstates, table;
+    response = await fetch('assets/FS_National_Forests.geojson');
+    nationalforests = await response.json();
+    response = await fetch('assets/capitals.geojson');
+    capitals = await response.json();
+    response = await fetch('assets/us-states.json');
+    usstates = await response.json();
 
 
     // Below is step 6 in the lab
     //load data to the map as new layers and table on the side.
     map.on('load', function loadingData() {
-        map.addSource('japan', {
+        map.addSource('usstates', {
             type: 'geojson',
-            data: japan
+            data: usstates
         });
     
         map.addLayer({
-            'id': 'japan-layer',
+            'id': 'usstates-layer',
             'type': 'fill',
-            'source': 'japan',
+            'source': 'usstates',
             'paint': {
-                'fill-color': '#0080ff', // blue color fill
+                'fill-color': '#A020F0', // purple color fill
                 'fill-opacity': 0.5
             }
         });
     
-        map.addSource('earthquakes', {
+        map.addSource('nationalforests', {
             type: 'geojson',
-            data: earthquakes
+            data: nationalforests
         });
     
         map.addLayer({
-            'id': 'earthquakes-layer',
-            'type': 'circle',
-            'source': 'earthquakes',
+            'id': 'nationalforests-layer',
+            'type': 'fill',
+            'source': 'nationalforests',
             'paint': {
-                'circle-radius': 8,
-                'circle-stroke-width': 2,
-                'circle-color': 'red',
-                'circle-stroke-color': 'white'
+                'fill-color': 'green', // green color fill
+                'fill-opacity': 1
             }
         });
     
+        map.addSource('capitals', {
+            type: 'geojson',
+            data: capitals
+        });
     
+        map.addLayer({
+            'id': 'capitals-layer',
+            'type': 'circle',
+            'source': 'capitals',
+            'paint': {
+                'circle-radius': 8,
+                'circle-stroke-width': 2,
+                'circle-color': 'blue',
+                'circle-stroke-color': 'white'
+            }
+        });
+
+        // map.addLayer({
+        //     'id': 'capitals-labels',
+        //     'type': 'symbol',
+        //     'source': 'capitals',
+        //     'paint': {
+        //         'symbol-placement':'point',
+        //         'text-anchor':'center',
+        //         'text-color':'white'
+        //         // 'text-fields'
+        //     }
+        // });
         
     
     });
@@ -63,30 +88,32 @@ async function geojsonFetch() {
     // step 7 in the lab
     table = document.getElementsByTagName("table")[0];
     let row, cell1, cell2, cell3;
-    for (let i = 0; i < earthquakes.features.length; i++) {
+    for (let i = 0; i < nationalforests.features.length; i++) {
         // Create an empty <tr> element and add it to the 1st position of the table:
         row = table.insertRow(-1);
         cell1 = row.insertCell(0);
         cell2 = row.insertCell(1);
         cell3 = row.insertCell(2);
-        cell1.innerHTML = earthquakes.features[i].properties.id;
-        cell2.innerHTML = earthquakes.features[i].properties.mag;
-        cell3.innerHTML = new Date(earthquakes.features[i].properties.time).toLocaleDateString(
-            "en-US");
+        cell1.innerHTML = nationalforests.features[i].properties.FORESTNAME;
+        cell2.innerHTML = nationalforests.features[i].properties.OBJECTID;
+        cell3.innerHTML = nationalforests.features[i].geometry.coordinates;
+        // cell3.innerHTML = new Date(nationalforests.features[i].properties.time).toLocaleDateString(
+            // "en-US");
     }
     
     // step 8 in the lab
-    let btn = document.getElementsByTagName("button")[0];
+    let btn = document.getElementsByTagName("button")[0]; //gets first button with tag button from the html, the 0 is for indexing purposes for if there is multiple buttons
     
     btn.addEventListener('click', sortTable);
 
-    
+
 };
 
 // define the function to sort table
 function sortTable(e) {
     let table, rows, switching, i, x, y, shouldSwitch;
     table = document.getElementsByTagName("table")[0];
+    //FOR TESTING: console.log(table)
     switching = true;
     /*Make a loop that will continue until
     no switching has been done:*/
@@ -101,10 +128,21 @@ function sortTable(e) {
             shouldSwitch = false;
             /*Get the two elements you want to compare,
             one from current row and one from the next:*/
-            x = parseFloat(rows[i].getElementsByTagName("td")[1].innerHTML);
-            y = parseFloat(rows[i + 1].getElementsByTagName("td")[1].innerHTML);
+            //ORIGINAL: x = parseFloat(rows[i].getElementsByTagName("td")[0].innerHTML);
+            //ORIGINAL: y = parseFloat(rows[i + 1].getElementsByTagName("td")[0].innerHTML);
+            x = rows[i].getElementsByTagName("td")[0].innerHTML; //DO NOT NEED parseFloat here as it takes string and parses it into a float
+            y = rows[i + 1].getElementsByTagName("td")[0].innerHTML; //0 is the table data index, aka the column number here, and we want first column aka index 0
+
+            //FOR TESTING: console.log(x);
+            //FOR TESTING: console.log(y);
+
             //check if the two rows should switch place:
-            if (x < y) {
+            if (x.toLowerCase() > y.toLowerCase()) {
+            //in line above, removed .innerHTML as was not needed and caused errors as would not even be able to reach/parse 
+            //this if statement correctly, it thought toLowerCase was the issue, but .innerHTML was the issue. Not fully sure why it was unneeded though 
+            //or why it caused issues, I think it was as we already had x and y, didn't need to attempt to grab the html again?
+
+            // changed line above to this for alphabetical sorting, see this source: https://www.w3schools.com/howto/howto_js_sort_table.asp
                 //if so, mark as a switch and break the loop:
                 shouldSwitch = true;
                 break;
